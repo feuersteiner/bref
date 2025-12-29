2ea5e333d0ec6bb7cb3e153191306c72902dd5d2
feuersteiner <safwen.bouali@outlook.com>
2025-12-29 16:12:32 +0100

    add font imports for material symbols to the library

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,5 @@
+// Fonts - bundled with the library
+import "@fontsource-variable/material-symbols-outlined";
+import "@fontsource-variable/material-symbols-rounded";
+
 // Reexport your entry components here
