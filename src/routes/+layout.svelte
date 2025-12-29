c32be5220f0c07dfc7108c81662eee6d5c51009a
feuersteiner <safwen.bouali@outlook.com>
2025-12-29 16:37:32 +0100

    fix: standardize quote style for font imports and font-family declaration

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,12 @@
 <script>
-	import "@fontsource-variable/jost";
+	import '@fontsource-variable/jost';
+	import '@fontsource/meow-script';
 </script>
 
 <slot />
 
 <style>
 	:global(*) {
-		font-family: "Jost Variable", sans-serif;
+		font-family: 'Jost Variable', sans-serif;
 	}
 </style>
