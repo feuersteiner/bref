2d269c16248a833b06909d8e6a73d2d4b446e51f
feuersteiner <safwen.bouali@outlook.com>
2025-12-30 22:08:24 +0100

    feat: add icon component

---
+++ src/lib/base/icon/icon.svelte
@@ -0,0 +1,78 @@
+<script lang="ts">
+	import '@fontsource-variable/material-symbols-rounded/fill.css';
+	import '@fontsource-variable/material-symbols-outlined';
+	import type { SvelteHTMLElements } from 'svelte/elements';
+	import type { IconProps } from './types.ts';
+
+	const {
+		name,
+		filled = false,
+		size = 'medium',
+		color = 'foreground',
+		...rest
+	}: IconProps & SvelteHTMLElements['span'] = $props();
+</script>
+
+<span {...rest} class:filled class={`${size} ${color}`} aria-hidden="true">
+	{name}
+</span>
+
+<style>
+	span {
+		font-family: 'Material Symbols Outlined Variable';
+		font-weight: normal;
+		font-style: normal;
+		display: inline-block;
+		line-height: 1;
+		text-transform: none;
+		letter-spacing: normal;
+		word-wrap: normal;
+		white-space: nowrap;
+		direction: ltr;
+		user-select: none;
+	}
+
+	.filled {
+		font-family: 'Material Symbols Rounded Variable';
+	}
+
+	.x-small {
+		font-size: 0.75rem;
+	}
+	.small {
+		font-size: 1rem;
+	}
+	.medium {
+		font-size: 1.5rem;
+	}
+	.large {
+		font-size: 2rem;
+	}
+	.x-large {
+		font-size: 3rem;
+	}
+	.foreground {
+		color: var(--color-foreground-base);
+	}
+	.primary {
+		color: var(--color-primary-base);
+	}
+	.secondary {
+		color: var(--color-secondary-base);
+	}
+	.success {
+		color: var(--color-success-base);
+	}
+	.warning {
+		color: var(--color-warning-base);
+	}
+	.danger {
+		color: var(--color-danger-base);
+	}
+	.background {
+		color: var(--color-background-base);
+	}
+	.info {
+		color: var(--color-info-base);
+	}
+</style>
---
+++ src/lib/base/icon/index.ts
@@ -0,0 +1,2 @@
+export { type IconName, type IconProps } from './types.ts';
+export { default as Icon } from './icon.svelte';
