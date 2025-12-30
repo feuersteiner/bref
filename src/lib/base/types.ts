4a844175480ee6bb4e808e44149c3833b54bcff7
feuersteiner <safwen.bouali@outlook.com>
2025-12-30 02:05:25 +0100

    feat: add types for Size, Color, and Variant in types.ts

---
+++ src/lib/base/types.ts
@@ -0,0 +1,3 @@
+export type Size = 'x-small' | 'small' | 'medium' | 'large' | 'x-large';
+export type Color = 'primary' | 'secondary' | 'success' | 'warning' | 'danger' | 'info';
+export type Variant = 'filled' | 'soft' | 'ghost';
