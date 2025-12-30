93058bb37d4fbb292f9d3c7ffbebd85f2ca8686c
feuersteiner <safwen.bouali@outlook.com>
2025-12-30 22:08:03 +0100

    feat: extend Color type to include foreground and background options

--- src/lib/base/types.ts
+++ src/lib/base/types.ts
@@ -1,3 +1,11 @@
 export type Size = 'x-small' | 'small' | 'medium' | 'large' | 'x-large';
-export type Color = 'primary' | 'secondary' | 'success' | 'warning' | 'danger' | 'info';
+export type Color =
+	| 'primary'
+	| 'secondary'
+	| 'success'
+	| 'warning'
+	| 'danger'
+	| 'info'
+	| 'foreground'
+	| 'background';
 export type Variant = 'filled' | 'soft' | 'ghost';
