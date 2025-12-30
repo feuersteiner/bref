5b17fd1b72c04e33c1362680d7a72e08bf9e99f3
feuersteiner <safwen.bouali@outlook.com>
2025-12-30 22:10:26 +0100

    feat: replace placeholder content with demo components and layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,38 @@
-<h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import Icon from '$lib/base/icon/icon.svelte';
+	import DemoHeader from '$lib/internal/demo-header.svelte';
+	import DemoIconSection from '$lib/internal/demo-icon-section.svelte';
+	import DemoSection from '$lib/internal/demo-section.svelte';
+	import DemoTypes from '$lib/internal/demo-types.svelte';
+</script>
+
+<main>
+	<DemoHeader />
+
+	<DemoSection title="Table of Contents">
+		<ul>
+			<li><a href="#icons">Icons</a></li>
+			<li><a href="#buttons">Buttons</a></li>
+		</ul>
+	</DemoSection>
+	<DemoTypes />
+	<DemoIconSection />
+</main>
+
+<style>
+	main {
+		width: 100vw;
+		min-height: 100vh;
+		height: fit-content;
+		display: flex;
+		align-items: center;
+		flex-direction: column;
+		gap: 2rem;
+		padding: 4rem;
+		background-color: #edebd7;
+		background-image: url("data:image/svg+xml,%3Csvg viewBox='0 0 200 200' xmlns='http://www.w3.org/2000/svg'%3E%3Cfilter id='noise'%3E%3CfeTurbulence type='fractalNoise' baseFrequency='0.8' numOctaves='4' stitchTiles='stitch'/%3E%3C/filter%3E%3Crect width='100%25' height='100%25' filter='url(%23noise)' opacity='0.4'/%3E%3C/svg%3E");
+		background-size: 200px 200px;
+		background-repeat: repeat;
+		background-blend-mode: overlay;
+	}
+</style>
