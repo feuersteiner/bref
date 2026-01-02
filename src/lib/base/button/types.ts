217bf9e020fdb62b8e85eeb3d2e3deb9a5868ef2
feuersteiner <safwen.bouali@outlook.com>
2026-01-02 18:10:48 +0100

    refactor: define button types and interfaces for improved component structure

---
+++ src/lib/base/button/types.ts
@@ -0,0 +1,21 @@
+import type { IconProps } from '../icon/types.ts';
+import type { Color, Size, Variant } from '../types.ts';
+
+export interface BaseButtonProps {
+	size?: Size;
+	color?: Color;
+	disabled?: boolean;
+	onClick: () => void;
+	variant?: Variant;
+}
+
+export interface IconButtonProps extends BaseButtonProps {
+	icon: Pick<IconProps, 'name' | 'filled' | 'ariaLabel'>;
+	rounded?: boolean;
+}
+
+export interface ButtonProps extends BaseButtonProps {
+	label: string;
+	wide?: boolean;
+	icon?: Pick<IconProps, 'name' | 'filled' | 'ariaLabel'>;
+}
