2db6a20f6da66c97eaa7aeaf0b4361938b24a5e3
feuersteiner <safwen.bouali@outlook.com>
2026-01-02 22:18:09 +0100

    feat: increment version to 0.0.10 and update button exports in index.ts

--- src/lib/base/button/index.ts
+++ src/lib/base/button/index.ts
@@ -1,3 +1,3 @@
-export * from './icon-button.svelte';
-export * from './button.svelte';
+export { default as IconButton } from './icon-button.svelte';
+export { default as Button } from './button.svelte';
 export * from './types.ts';
