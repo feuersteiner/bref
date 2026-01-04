cf8b75a7b786fa3e0418d1aca26380945d8c09ce
feuersteiner <safwen.bouali@outlook.com>
2026-01-04 15:00:37 +0100

    feat: restructure layout by adding Sidebar and Header components for improved page structure

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,12 +2,22 @@
 	import '@fontsource/spectral';
 	import '@fontsource/meow-script';
 	import Theme from '$lib/base/theme/theme.svelte';
+	import Sidebar from '$lib/internal/layout/sidebar/sidebar.svelte';
+	import Header from '$lib/internal/layout/header.svelte';
 
 	const { children } = $props();
+	// page
 </script>
 
 <Theme />
-{@render children()}
+
+<div>
+	<Sidebar />
+	<main>
+		<Header />
+		{@render children()}
+	</main>
+</div>
 
 <style>
 	:global(*) {
@@ -20,5 +30,53 @@
 	:global(h1) {
 		font-family: 'Meow Script', cursive;
 		font-size: 5rem;
+	}
+	:global(body) {
+		width: 100vw;
+		min-height: 100vh;
+		height: fit-content;
+		display: flex;
+		align-items: center;
+		flex-direction: column;
+		background-color: #edebd7;
+		background-image: url("data:image/svg+xml,%3Csvg viewBox='0 0 200 200' xmlns='http://www.w3.org/2000/svg'%3E%3Cfilter id='noise'%3E%3CfeTurbulence type='fractalNoise' baseFrequency='0.8' numOctaves='4' stitchTiles='stitch'/%3E%3C/filter%3E%3Crect width='100%25' height='100%25' filter='url(%23noise)' opacity='0.4'/%3E%3C/svg%3E");
+		background-size: 200px 200px;
+		background-repeat: repeat;
+		background-blend-mode: overlay;
+	}
+	:global(.container) {
+		display: flex;
+		gap: 1rem;
+		overflow: hidden;
+		flex-direction: column;
+		background-color: color-mix(in srgb, var(--color-background) 90%, transparent);
+		backdrop-filter: blur(2rem);
+		transition: all 0.2s ease;
+		border-radius: 2rem;
+		border: 1px solid color-mix(in srgb, var(--color-foreground) 10%, transparent);
+	}
+	:global(.container:hover) {
+		border: 1px solid color-mix(in srgb, var(--color-foreground) 20%, transparent);
+		box-shadow: 0 0 3rem color-mix(in srgb, var(--color-background) 50%, transparent);
+	}
+	div {
+		display: flex;
+		gap: 1rem;
+		max-width: 60rem;
+		height: 100vh;
+		max-height: 100vh;
+		width: 100%;
+		padding: 2rem;
+		/* overflow: hidden; */
+	}
+	main {
+		flex: 1;
+		height: 100%;
+		display: flex;
+		width: 100%;
+		overflow: auto;
+		flex-direction: column;
+		gap: 1rem;
+		background-color: transparent;
 	}
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,27 +11,25 @@
 	import DemoTypes from '$lib/internal/demo-types.svelte';
 </script>
 
-<main>
-	<DemoHeader />
-	<DemoSection title="Table of Contents">
-		<ul>
-			<li><a href="#theming">Theming</a></li>
-			<li><a href="#types">Types</a></li>
-			<li><a href="#buttons">Buttons</a></li>
-			<li><a href="#icons">Icons</a></li>
-			<li><a href="#loading">Loading</a></li>
-			<li><a href="#tree-view">Tree View</a></li>
-		</ul>
-	</DemoSection>
-	<DemoTheming />
-	<DemoTypes />
-	<DemoIconSection />
-	<DemoButton />
-	<DemoIconButton />
-	<DemoLoading />
-	<DemoTreeView />
-	<DemoAuthor />
-</main>
+<DemoHeader />
+<DemoSection title="Table of Contents">
+	<ul>
+		<li><a href="#theming">Theming</a></li>
+		<li><a href="#types">Types</a></li>
+		<li><a href="#buttons">Buttons</a></li>
+		<li><a href="#icons">Icons</a></li>
+		<li><a href="#loading">Loading</a></li>
+		<li><a href="#tree-view">Tree View</a></li>
+	</ul>
+</DemoSection>
+<DemoTheming />
+<DemoTypes />
+<DemoIconSection />
+<DemoButton />
+<DemoIconButton />
+<DemoLoading />
+<DemoTreeView />
+<DemoAuthor />
 
 <style>
 	a {
@@ -44,20 +42,5 @@
 
 	a:hover {
 		font-weight: bold;
-	}
-	main {
-		width: 100vw;
-		min-height: 100vh;
-		height: fit-content;
-		display: flex;
-		align-items: center;
-		flex-direction: column;
-		gap: 2rem;
-		padding: 4rem;
-		background-color: #edebd7;
-		background-image: url("data:image/svg+xml,%3Csvg viewBox='0 0 200 200' xmlns='http://www.w3.org/2000/svg'%3E%3Cfilter id='noise'%3E%3CfeTurbulence type='fractalNoise' baseFrequency='0.8' numOctaves='4' stitchTiles='stitch'/%3E%3C/filter%3E%3Crect width='100%25' height='100%25' filter='url(%23noise)' opacity='0.4'/%3E%3C/svg%3E");
-		background-size: 200px 200px;
-		background-repeat: repeat;
-		background-blend-mode: overlay;
 	}
 </style>
