b13a4e005699bf6b0701633837be09e05508448f
feuersteiner <safwen.bouali@outlook.com>
2026-01-04 17:21:59 +0100

    fix: update svelte.config.js to conditionally set base path for development

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,16 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
+const dev = process.argv.includes('dev');
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+		paths: {
+			base: dev ? '' : '/bref'
+		}
 	},
 	compilerOptions: {
 		runes: true
