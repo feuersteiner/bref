f84db3de0bc4d0574e89bf2c864d6b728d6c63cc
feuersteiner <safwen.bouali@outlook.com>
2026-01-04 14:58:06 +0100

    feat: add error handling component for displaying error messages

---
+++ src/routes/+error.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import { page } from '$app/state';
+</script>
+
+<div>
+	<h1>
+		{page.status}
+	</h1>
+	<p>
+		{page.error?.message || 'An unexpected error occurred.'}
+	</p>
+</div>
+
+<style>
+	div {
+		display: flex;
+		width: 100%;
+		height: 100%;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		gap: 1rem;
+	}
+</style>
